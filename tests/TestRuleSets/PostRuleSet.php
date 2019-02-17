3944cbc2b31e65c64926e19af15c089a3b8bd816
Travis Elkins <travis.elkins@gmail.com>
2019-02-17 23:38:46 +0100

    Minor cleanup of test class.

--- tests/TestRuleSets/PostRuleSet.php
+++ tests/TestRuleSets/PostRuleSet.php
@@ -29,8 +29,6 @@ class PostRuleSet extends AbstractResourceRuleSet
             'subject' => [
                 'required',
             ],
-            // 'body' => [
-            // ],
         ];
     }
 
